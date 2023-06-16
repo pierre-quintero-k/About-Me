60e3bf68b7c7c915dc60fd7ca0b3ef705be7f7d7
PierreQuintero-eventtia <pierre@eventtia.com>
2023-06-15 21:26:39 -0400

    funcionality email and linkedin added

--- app/src/main/java/com/example/aboutme/MainActivity.java
+++ app/src/main/java/com/example/aboutme/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.aboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.aboutme.databinding.ActivityMainBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        FloatingActionButton btn_float = binding.floatBtn;
+
+        btn_float.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/aboutme/MainActivity2.java
+++ app/src/main/java/com/example/aboutme/MainActivity2.java
@@ -2,13 +2,78 @@ package com.example.aboutme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.aboutme.databinding.ActivityMain2Binding;
+import com.example.aboutme.databinding.ActivityMainBinding;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private ActivityMain2Binding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        getSupportActionBar().hide();
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        Button link_btn = binding.linkedinBtn;
+        Button what_btn = binding.whatBtn;
+        EditText message_text = binding.messageEmail;
+        Button email_btn = binding.emailBtn;
+
+        email_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = message_text.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "pierrequintero@gmail.com" });
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Mensaje desde la App");
+                intent.putExtra(Intent.EXTRA_TEXT, message);
+
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        what_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = "+56971978769";
+                String whatsappUrl = "https://api.whatsapp.com/send?phone=" + phoneNumber;
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(whatsappUrl));
+                startActivity(intent);
+            }
+        });
+
+        link_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Abrir el perfil de LinkedIn en la aplicación oficial de LinkedIn
+                String linkedinProfileId = "pierre-quintero";
+                String linkedinAppUrl = "linkedin://profile/" + linkedinProfileId;
+
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinAppUrl));
+                    intent.setPackage("com.linkedin.android");
+                    startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    // La aplicación de LinkedIn no está instalada, abrir en el navegador
+                    String linkedinWebUrl = "https://www.linkedin.com/in/pierre-quintero";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(linkedinWebUrl));
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
